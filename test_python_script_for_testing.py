2d21b76c0b676b7ec86a8a0d7bc9fcc36cd50572
Me <ghost_vision@abv.bg>
2014-03-19 13:32:09 +0200

    die neueste Version

--- test_python_script_for_testing.py
+++ test_python_script_for_testing.py
@@ -3,22 +3,22 @@ import sys
 import os
 
 def main():
-	if(len(sys.argv) == 2):
-		file_directory = sys.argv[1]
-		directory_content = os.listdir(file_directory) #sydyrjanieto na papkata
-		for i in directory_content:
-			if ("." not in i): #proverqvame, dali i e papka ili fail
-				os.chdir(faille_directory + "/" + i)
-				print("Testing " + i + ":")
-				call("python3.3 tests.py", shell = True)
-				os.chdir(file_directory)
-				print()
-				print()
-				print()
-	elif(len(sys.argv)>2):
-		return "Too many arguments given!"
-	else:
-		return "No arguments given!"
+    if(len(sys.argv) == 2):
+        file_directory = sys.argv[1]
+        directory_content = os.listdir(file_directory) #sydyrjanieto na papkata
+        for i in directory_content:
+            if ("." not in i): #proverqvame, dali i e papka ili fail
+                os.chdir(file_directory + "/" + i)
+                print("Testing " + i + ":")
+                call("python3.3 tests.py", shell = True)
+                os.chdir(file_directory)
+                print()
+                print()
+                print()
+    elif(len(sys.argv)>2):
+        return "Too many arguments given!"
+    else:
+        return "No arguments given!"
 
 if __name__ == '__main__':
-		main()
+        main()
